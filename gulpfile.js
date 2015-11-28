a5e394235532c99533e2bb7f5c7d2877478acb14
Eddie Lee <eddie@funktroniclabs.com>
2015-11-28 09:58:44 -0800

    more tweaking gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,10 @@
 var gulp  = require('gulp'),
     gutil = require('gulp-util'),
     jshint = require('gulp-jshint'),
-    sass   = require('gulp-sass'),
+	sass   = require('gulp-sass'),
     browserSync = require('browser-sync'),
+    clean = require('gulp-clean'),
+    runSequence = require('run-sequence'),
     inject = require('gulp-inject')
     ;
 
@@ -13,61 +15,71 @@ var reload = browserSync.reload;
 
 var pathsSrc = {
 	html: 'source/index.html',
-	javascript: ['source/javascript/**/*.js'],
+	js: ['source/js/**/*.js'],
 	lib: ['source/lib/*.js'],
 	scss: ['source/scss/**/*.scss'],
 };
 
 var pathsDst = {
-	html: 'public',
-	javascript: 'public/javascript/',
-	scss: 'public/assets/stylesheets',
+	root: 'public/',
+	html: 'public/',
+	js: 'public/js/',
+	lib: 'public/lib/',
+	scss: 'public/css/',
 };
 
 ////////////////////////////////////////////////////////////
 
-// create a default task and just log a message
-gulp.task('default', function() {
-  return gutil.log('Gulp is running!')
-});
-
-gulp.task('dependenttask', ['mytask'], function() {
-  //do stuff after 'mytask' is done.
-});
-
 gulp.task('build-html', function(){
-    return gulp.src(pathsSrc.html)
-        .pipe(inject(gulp.src(pathsSrc.lib, {read: false}), {name: 'lib'}))
-        .pipe(gulp.dest( pathsDst.html ))
-        .pipe(inject(gulp.src(pathsSrc.javascript, {read: false}), {relative: true}))
-        .pipe(gulp.dest( pathsDst.html ))
-        .pipe(inject(gulp.src(pathsSrc.scss, {read: false}), {relative: true}))
-        .pipe(gulp.dest(pathsDst.html ));
-});
 
-/*gulp.task('build-html', function() {
-  gulp.src(pathsSrc.html).pipe(gulp.dest(pathsDst.html));
-});*/
+	var target = pathsDst.html;
+	var sources = gulp.src([pathsDst.js+'*.js', pathsDst.scss+'*.css'], {read: false});
+	var sourcesLib = gulp.src(pathsDst.lib+'*.js', {read: false});
+
+    return gulp.src(pathsSrc.html)
+        .pipe(gulp.dest(target))
+        .pipe(inject(sourcesLib, {relative:true, name: 'lib'}))
+        .pipe(gulp.dest(target))
+        .pipe(inject(sources, {relative: true}))
+        .pipe(gulp.dest(target))
+});
 
 gulp.task('build-js', function() {
-  return gulp.src(pathsSrc.javascript)
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(gulp.dest(pathsDst.javascript));
+	// pipe libs
+	gulp.src(pathsSrc.lib).pipe(gulp.dest(pathsDst.lib));
+
+	// pipe rest of 
+	return gulp.src(pathsSrc.js)
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish'))
+		.pipe(gulp.dest(pathsDst.js)) ;
 });
 
 gulp.task('build-scss', function() {
   return gulp.src(pathsSrc.scss)
     .pipe(sass())
-    .pipe(gulp.dest(pathsDst.stylesheets));
+    .pipe(gulp.dest(pathsDst.scss));
+});
+
+gulp.task('build-clean', function () {
+  return gulp.src(pathsDst.root, {read: false})
+    .pipe(clean());
+});
+
+gulp.task('build' , function() {
+	return runSequence(
+		'build-clean',
+		['build-scss', 'build-js'],
+		 'build-html'
+	);
 });
 
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function() {
   gulp.watch(pathsSrc.html, ['build-html']);
-  gulp.watch(pathsSrc.javascript, ['build-js']);
+  gulp.watch(pathsSrc.js, ['build-js']);
   gulp.watch(pathsSrc.scss, ['build-scss']);
-  gulp.watch([pathsSrc.javascript, ], ['inject-index']);
+  gulp.watch([pathsSrc.js, ], ['inject-index']);
 });
 
 gulp.task('serve', function() {
@@ -77,5 +89,10 @@ gulp.task('serve', function() {
     }
   });
 
-  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {cwd: 'public'}, reload);
+  gulp.watch(['*.html', 'css/*.css', 'js/*.js'], {cwd: 'public'}, reload);
+});
+
+// create a default task and just log a message
+gulp.task('default', function() {
+	runSequence( 'build', 'serve' );
 });
