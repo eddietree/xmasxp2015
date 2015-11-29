2390740d7d1e180ec659a0dde1d00c766a5f1e11
Eddie Lee <eddie@funktroniclabs.com>
2015-11-29 06:40:16 -0800

    gulp reload after watch

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,14 @@
 var gameDir = 'source/js/game/xmas2015';
 
 // grab our gulp packages
-var gulp  = require('gulp'),
-    gutil = require('gulp-util'),
-    jshint = require('gulp-jshint'),
-	sass   = require('gulp-sass'),
-    browserSync = require('browser-sync'),
-    clean = require('gulp-clean'),
-    runSequence = require('run-sequence'),
-    inject = require('gulp-inject')
-    ;
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	jshint = require('gulp-jshint'),
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync'),
+	clean = require('gulp-clean'),
+	runSequence = require('run-sequence'),
+	inject = require('gulp-inject');
 
 var reload = browserSync.reload;
 
@@ -33,66 +32,72 @@ var pathsDst = {
 
 ////////////////////////////////////////////////////////////
 
-gulp.task('build-html', function(){
+gulp.task('build-html', function() {
 
 	var target = pathsDst.html;
-	var sources = gulp.src([pathsDst.js+'**/*.js', pathsDst.scss+'*.css'], {read: false});
-	var sourcesLib = gulp.src(pathsDst.lib+'*.js', {read: false});
+	var sources = gulp.src([pathsDst.js + '**/*.js', pathsDst.scss + '*.css'], {
+		read: false
+	});
+	var sourcesLib = gulp.src(pathsDst.lib + '*.js', {
+		read: false
+	});
 
-    return gulp.src(pathsSrc.html)
-        .pipe(gulp.dest(target))
-        .pipe(inject(sourcesLib, {relative:true, name: 'lib'}))
-        .pipe(gulp.dest(target))
-        .pipe(inject(sources, {relative: true}))
-        .pipe(gulp.dest(target))
+	return gulp.src(pathsSrc.html)
+		.pipe(gulp.dest(target))
+		.pipe(inject(sourcesLib, {
+			relative: true,
+			name: 'lib'
+		}))
+		.pipe(gulp.dest(target))
+		.pipe(inject(sources, {
+			relative: true
+		}))
+		.pipe(gulp.dest(target))
 });
 
 gulp.task('build-js', function() {
 	// pipe libs
 	gulp.src(pathsSrc.lib).pipe(gulp.dest(pathsDst.lib));
 
-	// pipe rest of 
+	// pipe js
 	return gulp.src(pathsSrc.js)
 		.pipe(jshint())
 		.pipe(jshint.reporter('jshint-stylish'))
-		.pipe(gulp.dest(pathsDst.js)) ;
+		.pipe(gulp.dest(pathsDst.js));
 });
 
 gulp.task('build-scss', function() {
-  return gulp.src(pathsSrc.scss)
-    .pipe(sass())
-    .pipe(gulp.dest(pathsDst.scss));
+	return gulp.src(pathsSrc.scss)
+		.pipe(sass())
+		.pipe(gulp.dest(pathsDst.scss));
 });
 
-gulp.task('build-clean', function () {
-  return gulp.src(pathsDst.root, {read: false})
-    .pipe(clean());
+gulp.task('build-clean', function() {
+	return gulp.src(pathsDst.root, {
+			read: false
+		})
+		.pipe(clean());
 });
 
-gulp.task('build' , function(callback) {
+gulp.task('build', function(callback) {
 	runSequence(
-		'build-clean',
-		['build-scss', 'build-js'],
-		 'build-html',
-		 callback
+		'build-clean', ['build-scss', 'build-js'],
+		'build-html',
+		callback
 	);
 });
 
 // configure which files to watch and what tasks to use on file changes
-gulp.task('watch', function() {
-  gulp.watch(pathsSrc.html, ['build-html']);
-  gulp.watch(pathsSrc.js, function() { runSequence('build-js', 'build-html'); });
-  gulp.watch(pathsSrc.scss, function() { runSequence('build-scss', 'build-html'); });
-});
+// and handles reload
+gulp.task('serve', function() {
 
-gulp.task('serve',  function() {
-  browserSync({
-    server: {
-      baseDir: 'public'
-    }
-  });
+	browserSync({
+		server: {baseDir: 'public'}
+	});
 
-  gulp.watch(['*.html', 'css/*.css', 'js/**/*.js'], {cwd: 'public'}, reload);
+	gulp.watch(pathsSrc.html, ['build-html', reload]);
+	gulp.watch(pathsSrc.js, function() {runSequence('build-js', 'build-html', reload); });
+	gulp.watch(pathsSrc.scss, function() {runSequence('build-scss', 'build-html', reload); });
 });
 
 // create a default task and just log a message
@@ -101,10 +106,10 @@ gulp.task('default', function(callback) {
 	//runSequence( 'build', 'serve', callback );
 
 	runSequence(
-		'build-clean',
+		'build-clean', 
 		['build-scss', 'build-js'],
-		 'build-html',
-		['serve', 'watch'],
-		 callback
+		'build-html',
+		'serve',
+		callback
 	);
 });
