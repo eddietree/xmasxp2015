fd9dd60364e4a25aaf82fd84b343517ab8b1fee5
Eddie Lee <eddie@funktroniclabs.com>
2015-12-03 01:30:54 -0800

    rendered grided lines

--- source/js/game/xmas2015/Game.js
+++ source/js/game/xmas2015/Game.js
@@ -8,15 +8,40 @@ Game.prototype.init = function() {
 	this.sceneManager.addScene('SceneTestBox', new SceneTestBox());
 	APP.scene.add(this.sceneManager);
 
+	this.debugObjects = new THREE.Object3D;
 	if ( SETTINGS.debug ) {
-		var origin = new THREE.Vector3( 0, 0, 0 );
-		var arrowX = new THREE.ArrowHelper( v3(1,0,0), origin, 1, 0xff0000 );
-		var arrowY = new THREE.ArrowHelper( v3(0,1,0), origin, 1, 0x00ff00 );
-		var arrowZ = new THREE.ArrowHelper( v3(0,0,1), origin, 1, 0x00ff00 );
-		this.sceneManager.add( arrowX );
-		this.sceneManager.add( arrowY );
-		this.sceneManager.add( arrowZ );
+		this.initDebugGfx();
 	}
+};
+
+Game.prototype.initDebugGfx = function() {
+	// arrows
+	var origin = new THREE.Vector3( 0, 0, 0 );
+	var arrowX = new THREE.ArrowHelper( v3(1,0,0), origin, 1, 0xff0000 );
+	var arrowY = new THREE.ArrowHelper( v3(0,1,0), origin, 1, 0x00ff00 );
+	var arrowZ = new THREE.ArrowHelper( v3(0,0,1), origin, 1, 0x00ff00 );
+	this.debugObjects.add( arrowX );
+	this.debugObjects.add( arrowY );
+	this.debugObjects.add( arrowZ );
+
+	// lines
+	var geometry = new THREE.Geometry();
+
+	var numLinesPerSide = 100;
+	var step = 10;
+	for( var x = -numLinesPerSide; x <= numLinesPerSide; x+=step)
+	{
+    	geometry.vertices.push( v3(x, 0, -numLinesPerSide) );
+    	geometry.vertices.push( v3(x, 0, +numLinesPerSide) );
+    	geometry.vertices.push( v3(-numLinesPerSide, 0, x) );
+    	geometry.vertices.push( v3(+numLinesPerSide, 0, x) );
+    }
+
+    var lineMaterial = new THREE.LineBasicMaterial({color:0xffffff});
+    var line = new THREE.LineSegments(geometry, lineMaterial);
+   	this.debugObjects.add(line);
+
+   	APP.scene.add(this.debugObjects);
 };
 
 Game.prototype.initGui = function() {
