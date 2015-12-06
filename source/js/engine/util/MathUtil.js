6697f9ab0bafd3235ee685c59035f4e1161d17fc
Eddie Lee <eddie@funktroniclabs.com>
2015-12-06 00:14:27 -0800

    spherical pos

--- source/js/engine/util/MathUtil.js
+++ source/js/engine/util/MathUtil.js
@@ -17,3 +17,11 @@ function clamp(minVal, maxVal, val)
 {
 	return Math.max(minVal, Math.min(val, maxVal));
 }
+
+function spherical( theta, phi, radius ) {
+	radius = radius | 1.0;
+
+	var x = radius * Math.cos( theta ) * Math.sin(phi);
+	var z = radius * Math.sin( theta ) * Math.sin(phi);
+	var y = radius * Math.cos(phi);
+};
